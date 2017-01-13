9ce5a0dded45921fbfb1f0a6ce7f58b9fad12213
Julie Pierre <julie.pierre3@gmail.com>
2017-01-13 18:32:13 +0100

    adde delete method and add 8 weeks slider

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -2,15 +2,11 @@ var available_slots = [];
 var dates_to_remove = [];
 
 $('.availabilities-slot.active').each(function(i, slot){
-  // console.log('toto');
-  // console.log($(slot));
   var date = $(slot).data('date');
   var datetime = $(slot).data('time');
   available_slot = new Date(datetime);
   // ATTENTION : AVAILABLE SLOT A UNE HEURE DE PLUS QUE CE QUI EST AFFICHÉ MAIS POUR AFFICHAGE, PAS UN PROBLEME
-  console.log(available_slot);
   available_slots.push(available_slot);
-  // console.log(available_slots);
 });
 
 $('.availabilities-slot').click(function(){
@@ -20,9 +16,7 @@ $('.availabilities-slot').click(function(){
     var date = $(this).data('date');
     var datetime = $(this).data('time');
     available_slot = new Date(datetime);
-    console.log(available_slot);
     available_slots.push(available_slot);
-    console.log(available_slots);
   };
   if (!$(this).hasClass('active')){
     // si on clique sur un slot actif c'est que l'on n'en veut plus --> on l'enlève du tableau "availability"
@@ -34,17 +28,13 @@ $('.availabilities-slot').click(function(){
     available_slots.splice(index, 1);
 
     // on le supprime également de la base de données car il y a peut-être déjà été ajouté
-    console.log(date_to_remove);
     dates_to_remove.push(date_to_remove);
-    console.log(dates_to_remove)
   };
 });
 
 $('#validation-calendrier').click(function(){
-  // console.log(available_slots);
   var availabilities = JSON.stringify(available_slots);
   var availabilities_to_remove = JSON.stringify(dates_to_remove);
-  // console.log(availabilities);
   $.ajax({
     type: "POST",
     url: "/calendar/",
@@ -53,70 +43,7 @@ $('#validation-calendrier').click(function(){
       console.log('Success');
     }
   });
+  $('.availabilities-slot.active').addClass('green-bg');
+  $('.availabilities-slot').not('active').addClass('dark-grey-bg');
 });
-
-///////////////// 2ème version avec Ajax dès click ////////////
-
-// $('.availabilities-slot').click(function(){
-//   $(this).toggleClass('active');
-//   if ($(this).hasClass('active')){
-//     var selection  = $(this);
-//     var date = selection.data('date');
-//     var year = date.split(",")[0];
-//     var month = date.split(",")[1];
-//     var day = date.split(",")[2];
-//     var time = selection.data('time');
-//     var hour = time.split(":")[0];
-//     var minutes = time.split(":")[1];
-//     var date_to_add = new Date(year, month - 1, day, hour, minutes);
-//     console.log(date_to_add);
-//     $.ajax({
-//       type: "POST",
-//       url: "/calendar/",
-//       data: {date: date_to_add},
-//       success: function(data) {
-//         console.log('Success');
-//       }
-//     });
-//   };
-//   if (!$(this).hasClass('active')){
-//     var selection  = $(this);
-//     var date = selection.data('date');
-//     var year = date.split(",")[0];
-//     var month = date.split(",")[1];
-//     var day = date.split(",")[2];
-//     var time = selection.data('time');
-//     var hour = time.split(":")[0];
-//     var minutes = time.split(":")[1];
-//     var date_to_remove = new Date(year, month - 1, day, hour, minutes);
-//     console.log(date_to_remove);
-//   };
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,11 +3,26 @@ require 'json'
 class AvailabilitiesController < ApplicationController
   def new
     @week1 = []
+    @week2 = []
+    @week3 = []
+    @week4 = []
+    @week5 = []
+    @week6 = []
+    @week7 = []
+    @week8 = []
     (0..6).each do |i|
       @week1 << Date.today + i
+      @week2 << Date.today + i + 7
+      @week3 << Date.today + i + 7*2
+      @week4 << Date.today + i + 7*3
+      @week5 << Date.today + i + 7*4
+      @week6 << Date.today + i + 7*5
+      @week7 << Date.today + i + 7*6
+      @week8 << Date.today + i + 7*7
     end
+    @week = [@week1, @week2, @week3, @week4, @week5, @week6, @week7, @week8].flatten
     @slots = {}
-    @week1.each do |d|
+    @week.each do |d|
       @slots[d] = []
       (0..9).each do |j|
         @slots[d] << DateTime.new(d.year, d.month, d.day, 9 + j)
